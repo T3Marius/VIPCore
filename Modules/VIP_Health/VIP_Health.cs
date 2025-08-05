7c84695b0911fd007664c7a2da2fea61c6a4f318
T3Marius <168598779+T3Marius@users.noreply.github.com>
2025-08-05 10:52:56 +0300

    feat: added vip health module

---
+++ Modules/VIP_Health/VIP_Health.cs
@@ -0,0 +1,62 @@
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API;
+using VipCoreApi;
+
+namespace VIPCore;
+
+public class VIPHealth : BasePlugin
+{
+    public override string ModuleAuthor => "T3Marius";
+    public override string ModuleName => "[VIP] Health";
+    public override string ModuleVersion => "1.0.0";
+    public static IVipCoreApi VipApi = null!;
+
+    public override void OnAllPluginsLoaded(bool hotReload)
+    {
+        VipApi = IVipCoreApi.Capability.Get() ?? throw new Exception("VipApi not found");
+        VipApi.RegisterFeature(new VIP_Health(this));
+    }
+}
+
+public class VIP_Health : IVipFeatureBase
+{
+    private readonly BasePlugin _plugin;
+    public string FeatureName => "FeatureName";
+    private IVipCoreApi VipApi => VIPHealth.VipApi;
+
+    public void OnFeatureLoaded()
+    {
+        // Feature loaded logic here
+
+    }
+
+    public VIP_Health(BasePlugin plugin)
+    {
+        _plugin = plugin;
+        _plugin.RegisterEventHandler<EventPlayerSpawn>(OnPlayerSpawn);
+    }
+
+
+    private HookResult OnPlayerSpawn(EventPlayerSpawn @event, GameEventInfo info)
+    {
+        CCSPlayerController? player = @event.Userid;
+        if (player == null || player.PlayerPawn.Value == null)
+            return HookResult.Continue;
+
+        if (!VipApi.IsPlayerVip(player.SteamID) && !VipApi.IsPlayerFeatureEnabled(player.SteamID, FeatureName))
+            return HookResult.Continue;
+
+        var health = VipApi.GetFeatureValue<int>(player.SteamID, FeatureName)!.Value;
+
+        if (health > 0)
+        {
+            player.PlayerPawn.Value.Health = health;
+            player.PlayerPawn.Value.MaxHealth = player.PlayerPawn.Value.Health;
+
+            Utilities.SetStateChanged(player, "CBaseEntity", "m_iHealth");
+            Utilities.SetStateChanged(player, "CBaseEntity", "m_iMaxHealth");
+        }
+
+        return HookResult.Continue;
+    }
+}
